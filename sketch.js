51bdfd212769b00add644aa71da4ff32862efa74
Aarav77 <64909432+Aarav77@users.noreply.github.com>
2020-05-20 20:36:00 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -37,7 +37,30 @@ function setup() {
 
 	//Create a Ground
 	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
+	 World.add(world, ground);
+	 
+	 boxPosition=width/2-100
+ 	boxY=610;
+
+
+ 	boxleftSprite=createSprite(boxPosition, boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxLeftBody = Bodies.rectangle(boxPosition+20, boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxLeftBody);
+
+ 	boxBase=createSprite(boxPosition+100, boxY+40, 200,20);
+ 	boxBase.shapeColor=color(255,0,0);
+
+ 	boxBottomBody = Bodies.rectangle(boxPosition+100, boxY+45-20, 200,20 , {isStatic:true} );
+ 	World.add(world, boxBottomBody);
+
+ 	boxleftSprite=createSprite(boxPosition+200 , boxY, 20,100);
+ 	boxleftSprite.shapeColor=color(255,0,0);
+
+ 	boxRightBody = Bodies.rectangle(boxPosition+200-20 , boxY, 20,100 , {isStatic:true} );
+ 	World.add(world, boxRightBody);
+
 
 
 	Engine.run(engine);
@@ -55,7 +78,19 @@ function draw() {
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
+	if (keyCode === LEFT_ARROW) {
+
+		helicopterSprite.x=helicopterSprite.x-20;    
+		translation={x:-20,y:0}
+		Matter.Body.translate(packageBody, translation)
+	
+	
+	  } else if (keyCode === RIGHT_ARROW) {
+		helicopterSprite.x=helicopterSprite.x+20;
+		translation={x:20,y:0}
+		Matter.Body.translate(packageBody, translation)
+	  }
+	else if (keyCode === DOWN_ARROW) {
     Matter.Body.setStatic(packageBody,false);
     
   }
